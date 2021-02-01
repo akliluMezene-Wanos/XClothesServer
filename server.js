8acdcee5bc00123d2f05310e8220076055043f04
akliluMezene-wanos <aklilumezene@gmail.com>
2021-02-01 10:39:14 +0200

    omitted the clien.end() methed form the 'then' and 'catch' handlers because we dont want to reopen it now and then at each query

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
-const { Client } = require('pg')
+const { Client } = require("pg");
 const expres = require("express");
 
 app = expres();
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'xclothes',
-    password: 'klwklw',
-    port: 5432,
-  })
-  
-//  client.connect()
-//   client
-//   .query('SELECT * FROM manufacturer')
-//   .then(function (result) {
-//     console.log("success!");
-//     console.log(result.rowCount);
-//     client.end();
-//   })
-//   .catch(function (error) {
-//    console.log("ooops");
-//    console.log(error);
-//    client.end();
-//   });
+  user: "postgres",
+  host: "localhost",
+  database: "xclothes",
+  password: "klwklw",
+  port: 5432,
+});
+ 
+client.connect();
 
+app.get("/clothes", (req, resp) => {
   
-  app.get("/", (req, resp) => {
-      resp.write("IN GET");
+  client
+    .query("SELECT * FROM clothes")
+    .then(function (result) {
+      console.log("succes!");
+      console.log(result.rowCount);
+      console.log(result.rows);
+      resp.write("Success");
       resp.end();
-  }); 
+    })
+    .catch(function (error) {
+      console.log("ooops");
+      console.log(error);
+      resp.write("Failed");
+      resp.end();
+    });
+});
 
-  app.get("/clothes", (req, resp) => {
-      resp.write("In /clothes");
-      resp.end();
-  })
-  
-  const port = 3000;
-  app.listen(port, function () {console.log("server is started and listening to port " + port)});
-  
+app.get("/", (req, resp) => {
+  resp.write("In GET /");
+  resp.end();
+});
+
+const port = 3000;
+app.listen(port, function () {
+  console.log("server is started and listening to port " + port);
+});
