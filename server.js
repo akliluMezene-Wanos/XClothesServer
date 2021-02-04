d0a9a5881b70fc76a13ad8e62926f8daafbbdc5f
akliluMezene-wanos <aklilumezene@gmail.com>
2021-02-04 11:33:26 +0200

    saving clothes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const { Client } = require("pg");
-const expres = require("express");
-const { json } = require("body-parser");
+const express = require("express"); 
+//const { urlencoded } = require("body-parser");
 
-app = expres();
+app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
 const client = new Client({
   user: "postgres",
@@ -14,12 +18,41 @@ const client = new Client({
 
 client.connect();
 
+app.post("/clothes", (req, resp) => {
+  console.log("in /clothes POST"); 
+  
+  const myQuery = {
+    text: "INSERT INTO clothes (code, manufacturer, image_filename, short_description, description_more, promo) VALUES ($1, $2,$3,$4,$5,$6)",
+    values: [req.body.code, req.body.manufacturer, req.image_filename, req.body.short_description, req.body.description_more, req.body.promo]
+  };
+  client
+    .query(myQuery)
+    .then(function (result) {
+      console.log("succes!");
+      console.log(result.rowCount);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify("ok"));
+      resp.end();
+    })
+    .catch(function (error) {
+      console.log("ooops");
+      console.log(error);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify("Failed"));
+      resp.end();
+    });
+ });
+
 app.get("/clothes", (req, resp) => {
   let filterCode = req.query.filterCode;
   const myQuery = {
-    text:"SELECT * FROM clothes WHERE code LIKE $1",
-    values: ["%"+filterCode+"%"]
-  }
+    text: "SELECT * FROM clothes WHERE code LIKE $1",
+    values: ["%" + filterCode + "%"],
+  };
   client
     .query(myQuery)
     .then(function (result) {
