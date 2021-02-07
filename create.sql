76b4de9deba82049ff8de4eaf927bc926dd1c217
akliluMezene-wanos <aklilumezene@gmail.com>
2021-02-07 21:37:01 +0200

    added serever.sql and sample-data.sql

--- create.sql
+++ create.sql
@@ -2,5 +2,5 @@ CREATE TABLE clothes (id SERIAL PRIMARY KEY NOT NULL, code TEXT UNIQUE, manufact
 
 CREATE TABLE manufacturer (id SERIAL PRIMARY KEY NOT NULL, manufacturer_name TEXT NOT NULL, country VARCHAR(3), photo_link TEXT, description_short TEXT, more_description TEXT);
 
-CREATE TABLE ordZA(id SERIAL PRIMARY KEY NOT NULL, order_date DATE, cloth_id INT UNIQUE, quantity INT, customer_code VARCHAR(10) UNIQUE);
+CREATE TABLE orders (id SERIAL PRIMARY KEY NOT NULL, order_date DATE, cloth_id INT UNIQUE, quantity INT, customer_code VARCHAR(10) UNIQUE);
 
